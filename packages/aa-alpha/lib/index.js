2ba18d246ca1dcb8d537c9447ee5d1330bad563d
olivia osby <olivia.osby@gmail.com>
2021-11-02 15:24:55 -0700

    inital commit

---
+++ packages/aa-alpha/lib/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sayHello = void 0;
+var sayHello = function (greeting) {
+    console.log("greeting", greeting);
+};
+exports.sayHello = sayHello;
+//# sourceMappingURL=index.js.map
